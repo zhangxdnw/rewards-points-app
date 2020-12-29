abaca6ea1aaf6c2572bbd3a1a00984e18c06872c
zhangxiandong <zxd13510164430@gmail.com>
2020-12-29 16:02:24 +0800

    mina config

--- app/src/main/java/cn/zxd/app/ui/MainActivity.kt
+++ app/src/main/java/cn/zxd/app/ui/MainActivity.kt
@@ -3,14 +3,15 @@ package cn.zxd.app.ui
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import cn.zxd.app.R
 import cn.zxd.app.databinding.ActivityMainBinding
-import cn.zxd.app.net.ApiUtils
 import cn.zxd.app.service.VMDaemonService
+import cn.zxd.app.work.FaceDetectWork
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : BaseActivity() {
 
-    lateinit var dataBinding:ActivityMainBinding
+    lateinit var dataBinding: ActivityMainBinding
     private val clickMax = 1
     private var clickCount = 0
 
@@ -19,6 +20,18 @@ class MainActivity : BaseActivity() {
         dataBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(dataBinding.root)
         startService(Intent(this, VMDaemonService::class.java))
+    }
+
+    override fun onStart() {
+        super.onStart()
+        GlobalScope.launch {
+            FaceDetectWork.detectingFace()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        FaceDetectWork.canceled = true
     }
 
     fun clickToSettings(view: View) {
---
+++ app/src/main/java/cn/zxd/app/work/FaceDetectWork.kt
@@ -0,0 +1,14 @@
+package cn.zxd.app.work
+
+object FaceDetectWork {
+
+    var canceled:Boolean = false
+
+    fun detectingFace() {
+        //TODO 调用native检测人脸 有可能要用到 camera preview
+        while (!canceled) {
+
+        }
+    }
+
+}
